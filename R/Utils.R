561647c798b5d4ef6de667bee76fb24805f9b0ed
Johannes Smolander <johannes.smolander@gmail.com>
2021-04-27 13:28:11 +0300

    Create Utils.R
    Create R directory and Utils.R file

---
+++ R/Utils.R
@@ -0,0 +1,16 @@
+#' Data simulated using PROSSTT simulator
+#'
+#' @docType data
+#'
+#' @usage data(prosstt1)
+#'
+#' @format rna.normalized, rna.raw and pseudotime
+#'
+#' @keywords datasets
+#'
+#' @source \url{https://zenodo.org/record/1443566#.XiL7FyPRaUl}
+#'
+#' @examples
+#' data(prosstt1)
+"prosstt1"
+
